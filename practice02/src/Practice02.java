aa25cbcbf0b7d0a6297cbabf9d7764037b142d39
yubinka <kayubin27@gmail.com>
2023-02-21 20:30:36 +0900

    Change-Id: I0000000000000000000000000000000000000000
    Change-Id: I949e7a9167ff67049b3407877b4f3e688ac51fb9

---
+++ practice02/src/Practice02.java
@@ -0,0 +1,20 @@
+//컴퓨터학과_20220740_가유빈
+import java.util.Scanner;
+
+public class Practice02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int width = 10;
+		int height = 20;
+		
+		System.out.print("가로 길이: " + width + "\n세로 길이: " + height);
+		System.out.println();
+		
+		System.out.print("사각형의 넓이: ");
+		System.out.print(width * height);
+	}
+
+}
